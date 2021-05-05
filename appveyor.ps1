b08471101f196086fc4650517222666a3ed00f2a
Kandrashin Denis <mail@lintest.ru>
2021-05-05 22:42:50 +0300

    Временно отключена сборка Linux

--- appveyor.ps1
+++ appveyor.ps1
@@ -8,21 +8,11 @@ $path = $env:APPVEYOR_BUILD_FOLDER
 $version = $env:APPVEYOR_BUILD_VERSION
 $postfix = '_' + $version -replace '\.', '-'
 
-$apiUrl = 'https://ci.appveyor.com/api'
-$data = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$account/$project/build/$version"
-$jobId = $data.build.jobs[0].jobId
-
-Invoke-RestMethod -Method Get -OutFile "$path\Linux.zip" `
-  -Uri "$apiUrl/buildjobs/$jobId/artifacts/AddIn.zip" 
-
-Expand-Archive -Force -Path "$path\Linux.zip" -DestinationPath $path
 Rename-Item "$path\lib${name}Win32.dll" "${name}Win32$postfix.dll"
 Rename-Item "$path\lib${name}Win64.dll" "${name}Win64$postfix.dll"
-Rename-Item "$path\lib${name}Lin32.so" "${name}Lin32$postfix.so"
-Rename-Item "$path\lib${name}Lin64.so" "${name}Lin64$postfix.so"
 
 $compress = @{
-  Path            = "$path\$name*.dll", "$path\$name*.so", "$path\manifest.xml"
+  Path            = "$path\$name*.dll", "$path\manifest.xml"
   DestinationPath = "$path\AddIn.zip"
 }
 Compress-Archive @compress
